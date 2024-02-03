17c83324c12068fe7566be859d186103c0d4c734
William Pognonec <william.pognonec@gmail.com>
2024-02-03 08:10:06 -0600

    Using collections exercises 11-15

---
+++ using_collections/ex_15.py
@@ -0,0 +1,18 @@
+pets = {
+    'Cat':  'Meow',
+    'Dog':  'Bark',
+    'Bird': 'Tweet',
+}
+
+keys = pets.keys()
+del pets['Dog']
+pets['Snake'] = 'Sssss'
+print(keys)
+
+# pets = {
+#     'Cat':  'Meow',
+#     'Bird': 'Tweet',
+#     'Snake': 'Sssss'
+# }
+
+# keys = dict_keys('Cat', 'Bird', 'Snake')
